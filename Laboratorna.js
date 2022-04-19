ea755ff5d41412ae9af9dffbff1677f19a333da7
SilverCrows <85475217+SilverCrows@users.noreply.github.com>
2022-04-19 14:54:37 +0300

    Update Laboratorna.js

--- Laboratorna.js
+++ Laboratorna.js
@@ -1,6 +1,7 @@
 const blessed = require('blessed');
 
 let a = [];
+let b = [];
 let height = 42;
 let width  = 42;
 
@@ -8,18 +9,19 @@ function init() {
   for (let i=0; i<height; i++){
     for (let j=0; j<width; j++){
       a[i*width + j] = 0;
+      b[i*width + j] = " ";
     }
   }  
 }
 
 function pprint() {
   for (let i=0; i<height; i++){
-    let ln = "";
+    var ln = "";
     for (let j=0; j<width; j++){
-      ln += a[i*width + j] ? "*" : ".";
+      ln += a[i*width + j] ? "*" : " ";
       ln += " ";
     }
-    console.log(ln);
+     //console.log(ln);
   }
 }
 
@@ -38,30 +40,44 @@ function step() {
       }
       if (isAlive && liveCount < 2) {
         a[i*width + j] = 0;
+        b[i*width + j] = " ";
       } else if (isAlive && (liveCount == 2 || liveCount == 3)) {
         a[i*width + j] = 1;
+        b[i*width + j] = "*";
       } else if (isAlive && liveCount > 3) {
         a[i*width + j] = 0;
+        b[i*width + j] = " ";
       } else if (!isAlive && liveCount == 3) {
         a[i*width + j] = 1;
+        b[i*width + j] = "*";
       } 
+      pprint();
     }
-  }  
+  } 
 }
 
 init();
 
-a[height+4] = 1;
-a[height+5] = 1;
-a[(2*height)+5] = 1;
-a[(3*height)+5] = 1;
-a[(4*height)+5] = 1;
-a[(6*height)+4] = 1;
-a[(6*height)+5] = 1;
-a[(7*height)+3] = 1;
-a[(7*height)+4] = 1;
-a[(8*height)+6] = 1;
-a[(9*height)+5] = 1;
+// a[height+4] = 1;
+// a[height+5] = 1;
+// a[(2*height)+5] = 1;
+// a[(3*height)+5] = 1;
+// a[(4*height)+5] = 1;
+// a[(6*height)+4] = 1;
+// a[(6*height)+5] = 1;
+// a[(7*height)+3] = 1;
+// a[(7*height)+4] = 1;
+// a[(8*height)+6] = 1;
+// a[(9*height)+5] = 1;
+
+for (let i =0; i < 256; i++) { 
+  let rand = 1 + Math.random() * (42 - 1);
+   rand = Math.round(rand);
+   a[rand*width + i] = 1;
+   a[(rand*height)+i] = 1;
+}
+
+
 
 
 var screen = blessed.screen({
@@ -93,11 +109,11 @@ var text = blessed.text({
   }
 });
 
-text.setText(a.join(''));
+text.setText(b.join(''));
 
 setInterval(() => {
   step();
-  text.setText(a.join(''));
+  text.setText(b.join(''));
   screen.render();
 }, 200);
 
